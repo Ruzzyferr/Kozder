8de5a366a70869b88d885cbcdfe4fed6f5d16ab2
Ruzgar Emir <bulutruzgarofficial@gmail.com>
2026-03-18 21:01:05 +0300

    proje basvuru linkleri

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/events/esc-gonullu-bulusmasi-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fevents%2Fesc-gonullu-bulusmasi-2026.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/bulgaristan4-esc.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbulgaristan4-esc.mdx&astroContentModuleFlag=true")],
-["src/content/projects/esc-dijital-mentorluk-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fesc-dijital-mentorluk-2026.mdx&astroContentModuleFlag=true")],
-["src/content/posts/esc-2026-basvuru-duyurusu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fesc-2026-basvuru-duyurusu.mdx&astroContentModuleFlag=true")]]);
+["src/content/events/esc-gonullu-bulusmasi-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fevents%2Fesc-gonullu-bulusmasi-2026.mdx&astroContentModuleFlag=true")],
+["src/content/posts/esc-2026-basvuru-duyurusu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fesc-2026-basvuru-duyurusu.mdx&astroContentModuleFlag=true")],
+["src/content/projects/esc-dijital-mentorluk-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fesc-dijital-mentorluk-2026.mdx&astroContentModuleFlag=true")]]);
 		
