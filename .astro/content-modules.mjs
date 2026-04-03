cde739d009f2cf8a1b9c8a6186987e5c8e5d77a7
Ruzgar Emir <bulutruzgarofficial@gmail.com>
2026-04-03 18:34:16 +0300

    New main page

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,12 @@
 
 export default new Map([
 ["src/content/projects/bulgaristan-erasmus.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbulgaristan-erasmus.mdx&astroContentModuleFlag=true")],
+["src/content/projects/power-of-stories.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpower-of-stories.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/bulgaristan4-esc.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbulgaristan4-esc.mdx&astroContentModuleFlag=true")],
-["src/content/events/esc-gonullu-bulusmasi-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fevents%2Fesc-gonullu-bulusmasi-2026.mdx&astroContentModuleFlag=true")],
+["src/content/stories/busra-akdemir.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstories%2Fbusra-akdemir.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/esc-2026-basvuru-duyurusu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fesc-2026-basvuru-duyurusu.mdx&astroContentModuleFlag=true")],
-["src/content/stories/ahmet-yilmaz-bulgaristan.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstories%2Fahmet-yilmaz-bulgaristan.mdx&astroContentModuleFlag=true")]]);
+["src/content/stories/huseyin-basak.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstories%2Fhuseyin-basak.mdx&astroContentModuleFlag=true")],
+["src/content/stories/mustafa-oksuz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstories%2Fmustafa-oksuz.mdx&astroContentModuleFlag=true")],
+["src/content/stories/sedef-dizlek-misir.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstories%2Fsedef-dizlek-misir.mdx&astroContentModuleFlag=true")],
+["src/content/events/esc-gonullu-bulusmasi-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fevents%2Fesc-gonullu-bulusmasi-2026.mdx&astroContentModuleFlag=true")]]);
 		
